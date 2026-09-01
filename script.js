9d081523a944596b1a85cf6c9a67bc0d9586c6fd
thaisfnnascimento-ops <thaisfnnascimento@gmail.com>
2026-09-01 13:15:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const formulario = document.getElementById("formCliente");
 const listaClientes = document.getElementById("listaClientes");
 
 let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+let indiceEditando = null;
 
-function mostrarClientes() {
+function mostrarClientes(filtro = "") {
 
     listaClientes.innerHTML = "";
 
     clientes.forEach((cliente, index) => {
+
+        if (
+            !cliente.nome.toLowerCase().includes(filtro.toLowerCase()) &&
+            !cliente.email.toLowerCase().includes(filtro.toLowerCase())
+        ) {
+            return;
+        }
 
         const div = document.createElement("div");
 
@@ -17,6 +25,10 @@ function mostrarClientes() {
                 E-mail: ${cliente.email}<br>
                 Telefone: ${cliente.telefone}
             </p>
+
+            <button onclick="editarCliente(${index})">
+                Editar
+            </button>
 
             <button onclick="excluirCliente(${index})">
                 Excluir
@@ -37,20 +49,46 @@ formulario.addEventListener("submit", function(event) {
     const email = document.getElementById("email").value;
     const telefone = document.getElementById("telefone").value;
 
-    clientes.push({
-        nome: nome,
-        email: email,
-        telefone: telefone
-    });
+    if (indiceEditando === null) {
+
+        clientes.push({
+            nome: nome,
+            email: email,
+            telefone: telefone
+        });
+
+        alert("Cliente cadastrado com sucesso!");
+
+    } else {
+
+        clientes[indiceEditando] = {
+            nome: nome,
+            email: email,
+            telefone: telefone
+        };
+
+        indiceEditando = null;
+
+        alert("Cliente atualizado com sucesso!");
+    }
 
     localStorage.setItem("clientes", JSON.stringify(clientes));
 
     formulario.reset();
 
     mostrarClientes();
-
-    alert("Cliente cadastrado com sucesso!");
 });
+
+function editarCliente(index) {
+
+    const cliente = clientes[index];
+
+    document.getElementById("nome").value = cliente.nome;
+    document.getElementById("email").value = cliente.email;
+    document.getElementById("telefone").value = cliente.telefone;
+
+    indiceEditando = index;
+}
 
 function excluirCliente(index) {
 
@@ -59,6 +97,8 @@ function excluirCliente(index) {
     localStorage.setItem("clientes", JSON.stringify(clientes));
 
     mostrarClientes();
+
+    alert("Cliente excluído!");
 }
 
 mostrarClientes();
